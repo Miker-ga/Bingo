3daefef3f70034c11e15b8e59e3538edf6a4b14c
José Beita <jo13bc@gmail.com>
2022-12-24 11:35:26 -0600

    Project

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <div id="mainstage" class="container is-fluid">
+      <BingoGame />
+    </div>
+  </div>
+</template>
+
+<script>
+import BingoGame from './components/BingoGame.vue'
+
+export default {
+  name: 'App',
+  components: {
+    BingoGame
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Helvetica, Arial, Avenir, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+}
+
+#mainstage {
+  padding: 3rem;
+  border-radius: 10px;
+  background-color: white;
+}
+</style>
